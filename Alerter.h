8c103b0dbcbbcdf01caecd992dda975ea5562d4a
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2021-10-23 20:05:26 +0530

    Update Alerter.h

--- Alerter.h
+++ Alerter.h
@@ -1,6 +1,8 @@
 #ifndef Alerter_h
 #define Alerter_h
 
+#include "stats.h"
+
 void emailAlerter(float Threshold,struct Stats computedStats);
 
 void ledAlerter(float Threshold,struct Stats computedStats);
