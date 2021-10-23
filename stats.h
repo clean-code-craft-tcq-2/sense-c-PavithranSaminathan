884bf960eaad85f42d328339463a0c3bb5015cb5
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2021-10-23 20:12:37 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -13,8 +13,8 @@ struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)(float maxThreshold, struct Stats computedStats);
 
-void emailAlerter(float maxThreshold, struct Stats computedStats);
-void ledAlerter(float maxThreshold, struct Stats computedStats);
+//void emailAlerter(float maxThreshold, struct Stats computedStats);
+//void ledAlerter(float maxThreshold, struct Stats computedStats);
 
 //alerter_funcptr alerters[2];
 
